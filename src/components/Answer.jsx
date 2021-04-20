468a51251190fa4e0bd29148e10adecd7c14bc49
gmc-millan <50713237+gmc-millan@users.noreply.github.com>
2021-04-20 11:16:47 -0400

    transferred files from other repo

---
+++ src/components/Answer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Answer = () => {
+  return (
+    <button className="answer">
+      <div className="circle">A</div>
+      <span>Text</span>
+    </button>
+  );
+};
+
+export default Answer;
