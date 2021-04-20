a12b93b5a1548d2d03dd0fb1ccf1368d96b179b0
gmc-millan <50713237+gmc-millan@users.noreply.github.com>
2021-04-20 11:19:18 -0400

    added questions file

---
+++ src/Questions.js
@@ -0,0 +1,84 @@
+const questions = [
+  {
+    q:
+      "The conducting passageways of the respiratory system include all of the following structures EXCEPT:",
+    a: "alveoli",
+  },
+  {
+    q:
+      "The respiratory conducting passageways perform all of the following functions EXCEPT:",
+    a: "exchange gases",
+  },
+  {
+    q: "Which one of the following terms does not apply to the nose:",
+    a: "nasopharynx",
+  },
+  {
+    q: "What is the role of mucus in the nasal cavity:",
+    a: "trap incoming bacteria and other foreign debris",
+  },
+  {
+    q: "The nasal cavity is separated from the oral cavity by:",
+    a: "both the hard and soft palate",
+  },
+  {
+    q: "Which one of the following bones does NOT contain paranasal sinuses:",
+    a: "mandible",
+  },
+  {
+    q: "Which one of the following is NOT a function of the paranasal sinuses:",
+    a: "they are olfactory receptors for smell",
+  },
+  {
+    q: "Tonsils that lie at the base of the tongue are called:",
+    a: "lingual tonsils",
+  },
+  {
+    q: "Following the removal of the larynx, a person would be unable to:",
+    a: "sneeze",
+  },
+  {
+    q: "The opening between the vocal cords is called the:",
+    a: "glottis",
+  },
+  {
+    q:
+      "The flap of elastic cartilage that protects food from entering the larynx when swallowing is the:",
+    a: "epiglottis",
+  },
+  {
+    q:
+      "Which one of the following structures is NOT part of the respiratory zone:",
+    a: "primary bronchi",
+  },
+  {
+    q:
+      "Exchange of both oxygen and carbon dioxide through the respiratory membrane occurs by:",
+    a: "simple diffusion",
+  },
+  {
+    q:
+      "The lipid molecule critical to lung function that coats the gas-exposed alveolar surfaces is called:",
+    a: "surfactant",
+  },
+  {
+    q: "Air moving in and out of the lungs is called:",
+    a: "pulmonary ventilation",
+  },
+  {
+    q:
+      "The gas exchange that occurs between blood and tissue cells at systemic capillaries is called:",
+    a: "internal respiration",
+  },
+  {
+    q:
+      "An emotionally-induced response during which air movement is similar to crying is:",
+    a: "laughing",
+  },
+  {
+    q: "A very deep inspiration that ventilates all alveoli is:",
+    a: "yawning",
+  },
+];
+
+export default questions;
